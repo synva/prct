10b0367d79c0bf2692627895c46a35e2c1a36b5f
synva <synvalonki@gmail.com>
2020-04-17 12:54:01 +0900

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'prct-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'prct';
+}
